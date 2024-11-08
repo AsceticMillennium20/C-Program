ed680dd68d276952fa2a97ad2b028046ad6b0900
Abhinav K <abhinavpriyak2021@gmail.com>
2024-11-08 20:59:00 +0530

    Add files via upload

---
+++ alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char ch,num;
+    printf("Enter values : ");
+    scanf("%c %c",&ch,&num);
+    if((ch>=97&&num<=122)||(ch>=65&&num<=90))
+    {
+        printf("ALPHABET");
+    }
+    else 
+    {
+        printf("NOT AN ALPHABET");
+    }
+    return 0;
+}
